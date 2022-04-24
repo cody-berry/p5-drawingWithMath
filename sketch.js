5164d4e5ffd8baf8ecae8d8a1a720f9c46583d24
Cody <cody.tian@icloud.com>
2022-04-24 12:47:53 -0700

    add 4x8 grid

--- sketch.js
+++ sketch.js
@@ -18,23 +18,34 @@ function setup() {
     cnv.parent('#canvas')
     colorMode(HSB, 360, 100, 100, 100)
     textFont(font, 14)
+    background(234, 34, 24)
 
     /* initialize instruction div */
     instructions = select('#ins')
     instructions.html(`<pre>
         [1,2,3,4,5] → no function
         z → freeze sketch</pre>`)
+
+    translate(width/2, 3*height/4)
+    drawMxNgrid(4, 8, 20)
+
     noLoop()
 }
 
-
+// s is the side length of the squares, m is how many squares to the right
+// we will draw, n is how many squares to the top we will draw
+function drawMxNgrid(m, n, s) {
+    stroke(0, 0, 100, 20)
+    noFill()
+    for (let i = 0; i <= m; i++) {
+        line(i*s, 0, i*s, -n*s)
+    }
+    for (let j = 0; j <= n; j++) {
+        line(0, -j*s, m*s, -j*s)
+    }
+}
 
 function draw() {
-    background(234, 34, 24)
-
-
-
-    // displayDebugCorner()
 }
 
 
